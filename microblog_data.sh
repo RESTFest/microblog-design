a6a07984fdcb24b78950daaecc48441e639e9c13
Mike Amundsen <mamund@yahoo.com>
2011-07-22 22:56:15 -0400

    tweaks

--- microblog_data.sh
+++ microblog_data.sh
@@ -20,17 +20,19 @@ curl -vX PUT http://localhost:5984/html5-microblog/mary -d @user-mary.json
 
 echo 'testing user views...'
 curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/users_all
-curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/users_by_id?startkey=\"ma\"\&endkey=\"ma\\u9999\"
+curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/users_by_id?startkey=\"m\"\&endkey=\"m\\u9999\"
 
 echo 'adding posts...'
 curl -vX POST http://localhost:5984/html5-microblog/ -d @post1-mamund.json
 curl -vX POST http://localhost:5984/html5-microblog/ -d @post2-mamund.json
 curl -vX POST http://localhost:5984/html5-microblog/ -d @post1-lee.json
 curl -vX POST http://localhost:5984/html5-microblog/ -d @benjamin-post1.json
+curl -vX POST http://localhost:5984/html5-microblog/ -d @post1-mary.json
 
 echo 'testing post views...'
 curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/posts_all?descending=true
 curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/posts_by_user?descending=true\&key=\"mamund\"
+curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/posts_search?startkey=\"ma\"\&endkey=\"ma\\u9999\"
 
 echo 'adding follows...'
 curl -vX POST http://localhost:5984/html5-microblog/ -d @follows-mamund.json
@@ -42,5 +44,6 @@ echo 'testing follow views...'
 curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/follows_user_is_following?include_docs=true\&key=\"mamund\"
 curl -v http://localhost:5984/html5-microblog/_design/microblog/_view/follows_is_following_user?include_docs=true\&key=\"mamund\"
 
+echo 'testing posts from followers view...'
 curl -vX POST http://localhost:5984/html5-microblog/_design/microblog/_view/posts_by_user -d @posts_by_user.json
 
